1dee6c35306d53d9a1ccdfed5acaaebfaaca1a9f
Max <bethibande@gmail.com>
2022-12-22 14:48:45 +0100

    StringUserPropertyImpl lock hinzugefügt, bugfix für #11

--- api/src/main/java/net/enderstone/api/impl/properties/StringUserPropertyImpl.java
+++ api/src/main/java/net/enderstone/api/impl/properties/StringUserPropertyImpl.java
@@ -5,10 +5,12 @@ import net.enderstone.api.common.properties.abstraction.StringUserProperty;
 import net.enderstone.api.repository.UserPropertyRepository;
 
 import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class StringUserPropertyImpl extends StringUserProperty {
 
     private final UserPropertyRepository repository;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public StringUserPropertyImpl(UserProperty key, UUID owner, String value, UserPropertyRepository repository) {
         super(key, owner, value);
@@ -17,7 +19,11 @@ public class StringUserPropertyImpl extends StringUserProperty {
 
     @Override
     public void set(String value) {
+        lock.lock();
+
         super.set(value);
         repository.setValue(getOwner(), getKey(), value);
+
+        lock.unlock();
     }
 }
