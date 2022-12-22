32cf56e1f0bf3405fca55ffd579512f1e0b134f0
Max <bethibande@gmail.com>
2022-12-22 14:45:41 +0100

    Cache für PlayerService

--- rest/src/main/java/net/enderstone/api/service/PlayerService.java
+++ rest/src/main/java/net/enderstone/api/service/PlayerService.java
@@ -2,21 +2,37 @@ package net.enderstone.api.service;
 
 import com.bethibande.web.beans.GlobalBean;
 import net.enderstone.api.common.EPlayer;
+import net.enderstone.api.common.cache.CacheBuilder;
+import net.enderstone.api.common.cache.CacheLifetimeType;
+import net.enderstone.api.common.cache.ICache;
+import net.enderstone.api.common.cache.StorageType;
+import net.enderstone.api.common.cache.ref.HeapReference;
 import net.enderstone.api.common.properties.IUserProperty;
 import net.enderstone.api.impl.EPlayerImpl;
 import net.enderstone.api.repository.PlayerRepository;
 
 import java.util.Collection;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class PlayerService extends GlobalBean {
 
     private final PlayerRepository repository;
     private final UserPropertyService userPropertyService;
 
+    private final ICache<UUID, EPlayer> playerCache;
+
     public PlayerService(final PlayerRepository repository, final UserPropertyService userPropertyService) {
         this.repository = repository;
         this.userPropertyService = userPropertyService;
+
+        this.playerCache = CacheBuilder.<UUID, EPlayer>build("players")
+                                       .setLifetime(5L, TimeUnit.MINUTES)
+                                       .setLifetimeType(CacheLifetimeType.ON_ACCESS)
+                                       .setStorageType(StorageType.HEAP)
+                                       .setWriter((k, v) -> new HeapReference<>(v))
+                                       .setSupplier(repository::get)
+                                       .create();
     }
 
     public boolean playerExists(UUID uuid) {
@@ -36,13 +52,15 @@ public class PlayerService extends GlobalBean {
     }
 
     public EPlayer getPlayerById(UUID id) {
-        EPlayer EPlayer = repository.get(id);
-        if(EPlayer == null) return null;
+        final EPlayer player = playerCache.get(id);
+        if(player == null) return null;
 
-        final Collection<IUserProperty<?>> properties = userPropertyService.getAllUserProperties(id);
-        EPlayer.setProperties(properties);
+        if(player.getProperties().isEmpty()) {
+            final Collection<IUserProperty<?>> properties = userPropertyService.getAllUserProperties(id);
+            player.setProperties(properties);
+        }
 
-        return EPlayer;
+        return player;
     }
 
     public void deletePlayer(UUID id) {
